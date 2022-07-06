c70ece0129b1bffb5685ae61313f8d42b26c4381
kwambiee <kwambokaj2.jk@gmail.com>
2022-07-06 13:05:58 +0300

    Add more tables with primary and foreign keys

--- schema.sql
+++ schema.sql
@@ -13,3 +13,19 @@ CREATE TABLE ANIMALS(
     ALTER TABLE animals DROP COLUMN id;
     ALTER TABLE animals ADD id SERIAL PRIMARY KEY;
 
+CREATE TABLE owners(
+    id SERIAL PRIMARY KEY,
+    full_name TEXT,
+    age INT);
+
+CREATE TABLE species(
+    id SERIAL PRIMARY KEY,
+    name TEXT);
+
+ALTER TABLE animals DROP COLUMN species;
+ALTER TABLE animals ADD species_id INT;
+ALTER TABLE animals ADD owner_id INT;
+
+ ALTER TABLE animals ADD CONSTRAINT fk_species FOREIGN KEY(species_id) REFERENCES species(id);
+ ALTER TABLE animals ADD CONSTRAINT fk_owners FOREIGN KEY(owner_id) REFERENCES owners(id);
+
